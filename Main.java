acd3f16b18c9071b18edb82b8dc4fadb5f3f985c
Prashant Pujari <prashantpujariwork@gmail.com>
2023-12-22 16:41:48 +0530

    Added new line to check jenkins

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
         int b=6;
         System.out.println("Hello World!!");
         System.out.println(a + "X" + b + "=" + a*b);
+        System.out.println("Added this new line to check if jenkins can detect it!!");
         
     }
 }
